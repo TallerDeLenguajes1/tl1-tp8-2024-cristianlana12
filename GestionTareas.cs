e95923ca650fb3f41e4b17079575526bd433fb04
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-05-28 12:42:35 -0300

    Agregando el inicio

--- GestionTareas.cs
+++ GestionTareas.cs
@@ -2,12 +2,20 @@ namespace GestionTareas
 {
     class Tareas
     {
-       private int idTarea;
-       private string descripcionTarea;
-       private int duracionTarea;
+        static int cont = 0;
+        private int idTarea;
+        private string? descripcionTarea;
+        private int duracionTarea;
+
 
         public int IdTarea { get => idTarea; set => idTarea = value; }
         public string DescripcionTarea { get => descripcionTarea; set => descripcionTarea = value; }
         public int DuracionTarea { get => duracionTarea; set => duracionTarea = value; }
+        public Tareas(string? Descripcion, int Duracion)
+        {
+            IdTarea = cont++;
+            DescripcionTarea = Descripcion;
+            DuracionTarea = Duracion;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using GestionTareas;
 
+int opcion = 999;
+List<Tareas> listaTareas = new List<Tareas>();
+List<Tareas> listaTareasRealizadas = new List<Tareas>();
+
+
+while (opcion != 0)
+{
+    Console.WriteLine("### Gestion de Tareas ###");
+    Console.WriteLine("1_Ingresar tareas");
+    Console.WriteLine("2_Modificar Tarea");
+    Console.WriteLine("3_Tarea Realizada");
+    Console.WriteLine("4_Mostrar Tareas");
+    Console.WriteLine("0_Finalizar Sistema de Gestion");
+    Console.WriteLine("Ingresar Opcion: ");
+    int.TryParse(Console.ReadLine(), out opcion);
+
+    switch (opcion)
+    {
+        case 1:
+            Console.WriteLine("Ingresar Tarea: ");
+            string descripcionTarea;
+            int tiempo;
+            Console.WriteLine("Ingresar la descripcion de la tarea");
+            descripcionTarea = Console.ReadLine();
+            Console.WriteLine("Ingresar el timepo de duracion de la tarea: ");
+            int.TryParse(Console.ReadLine(), out tiempo);
+            listaTareas.Add(new Tareas(descripcionTarea, tiempo));
+            break;
+        
+    }
+}
